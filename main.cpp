ea6610280ac48cb83a4f8222a75f68e701ec804c
MdRahimulHassan <ndc12112026@gmail.com>
2024-01-15 21:15:50 +0600

    created the wall and check the collition with the wall

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@
 using namespace std;
 
 void renderPlayer(SDL_Renderer* renderer, SDL_Rect player, int x, int y, int scale, vector<int>tailX, vector<int>tailY, int tailLength)
-{
+{ 
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
     player.w=scale;
     player.h=scale;
@@ -24,6 +24,51 @@ void renderPlayer(SDL_Renderer* renderer, SDL_Rect player, int x, int y, int sca
     player.y=y;
 
     SDL_RenderFillRect(renderer, &player);
+}
+
+void renderFood(SDL_Renderer* renderer, SDL_Rect food)
+{
+    SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+    SDL_RenderFillRect(renderer, &food);
+}
+
+void renderScore(SDL_Renderer* renderer, int tailLength, int scale, int wScale)
+{
+
+}
+
+bool checkCollision(int foodx, int foody, int playerx, int playery)
+{
+    return(playerx==foodx&&playery==foody);
+}
+
+pair<int, int>getFoodSpawn(vector<int>tailX, vector<int> tailY, int playerX, int playerY, int scale, int wScale, int tailLength)
+{
+    bool valid=false;
+    int x=0;
+    int y=0;
+    srand(time(0));
+    x=scale*(rand()%wScale);
+    y=scale*(rand()%wScale);
+    valid=true;
+
+    for(int i=0;i<tailLength;i++)
+    {
+        if((x==tailX[i]&&y==tailY[i])||(x=playerX&&y==playerY))
+        {
+            valid=false;
+        }
+    }
+
+    if(!valid)
+    {
+        pair<int, int>foodLoc;
+        foodLoc=make_pair(-100, -100);
+        return foodLoc;
+    }
+    pair<int, int>foodLoc;
+    foodLoc=make_pair(x,y);
+    return foodLoc;
 }
 
 int main(int argc, char* argv[])
