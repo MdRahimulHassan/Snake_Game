11c8500a358f70f7144c94ba78efa327958a7111
MdRahimulHassan <ndc12112026@gmail.com>
2024-01-22 00:25:01 +0600

    score and game over showing by using ttf

--- main.cpp
+++ main.cpp
@@ -3,11 +3,12 @@
 #include <cstdlib>
 #include <ctime>
 #include "inc/SDL.h"
+#include "inc/SDL_ttf.h"
 #undef main
 using namespace std;
 
 void renderPlayer(SDL_Renderer* renderer, SDL_Rect player, int x, int y, int scale, vector<int> tailX, vector<int> tailY, int tailLength) {
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
     player.w = scale;
     player.h = scale;
 
@@ -29,8 +30,26 @@ void renderFood(SDL_Renderer* renderer, SDL_Rect food) {
     SDL_RenderFillRect(renderer, &food);
 }
 
-void renderScore(int tailLength) {
-    printf("Score: %d\n", tailLength);
+void renderScore(SDL_Renderer* renderer, int tailLength, int scale, int wScale) {
+    SDL_Color Black = { 255, 255, 255 };
+
+    // Get the font used for displaying text
+    TTF_Font* font = TTF_OpenFont("arial.ttf", 10);
+    if (font == NULL) {
+        cout << "Font loading error: " << TTF_GetError() << endl;
+        return;
+    }
+
+    SDL_Surface* score = TTF_RenderText_Solid(font, (string("Score: ") + to_string(tailLength * 10)).c_str(), Black);
+    SDL_Texture* scoreMessage = SDL_CreateTextureFromSurface(renderer, score);
+    SDL_Rect scoreRect;
+    scoreRect.w = 100;
+    scoreRect.h = 30;
+    scoreRect.x = ((scale*wScale) / 2) - (scoreRect.w / 2);
+    scoreRect.y = 0;
+    SDL_RenderCopy(renderer, scoreMessage, NULL, &scoreRect);
+
+    TTF_CloseFont(font);
 }
 
 bool checkCollision(int foodx, int foody, int playerx, int playery) {
@@ -52,8 +71,16 @@ pair<int, int> getFoodSpawn(vector<int> tailX, vector<int> tailY, int playerX, i
             valid = false;
         }
     }
-    
-    if((x == 72 && y == 72)||(x == 96 && y == 72)||(x == 120 && y == 72)||(x == 144 && y == 72)||(x == 72 && y == 96)||(x == 72 && y == 120)||(x == 72 && y == 144)||(x == 480 && y == 72)||(x == 456 && y == 72)||(x == 432 && y == 72)||(x == 408 && y == 72)||(x == 480 && y == 96)||(x == 480 && y == 120)||(x == 480 && y == 144)||(x == 72 && y == 480)||(x == 96 && y == 480)||(x == 120 && y == 480)||(x == 144 && y == 480)||(x == 72 && y == 456)||(x == 72 && y == 432)||(x == 72 && y == 408)||(x == 480 && y == 480)||(x == 456 && y == 480)||(x == 432 && y == 480)||(x == 408 && y == 480)||(x == 480 && y == 456)||(x == 480 && y == 432)||(x == 480 && y == 408))
+
+    if((x == 72 && y == 72)||(x == 96 && y == 72)|| (x == 120 && y == 72)||
+    (x == 144 && y == 72)||(x == 72 && y == 96)||(x == 72 && y == 120)||
+    (x == 72 && y == 144)||(x == 480 && y == 72)||(x == 456 && y == 72)||
+    (x == 432 && y == 72)||(x == 408 && y == 72)||(x == 480 && y == 96)||
+    (x == 480 && y == 120)||(x == 480 && y == 144)||(x == 72 && y == 480)||
+    (x == 96 && y == 480)||(x == 120 && y == 480)||(x == 144 && y == 480)||
+    (x == 72 && y == 456)||(x == 72 && y == 432)||(x == 72 && y == 408)||
+    (x == 480 && y == 480)||(x == 456 && y == 480)||(x == 432 && y == 480)||
+    (x == 408 && y == 480)||(x == 480 && y == 456)||(x == 480 && y == 432)||(x == 480 && y == 408))
     {
         valid=false;
     }
@@ -70,9 +97,76 @@ pair<int, int> getFoodSpawn(vector<int> tailX, vector<int> tailY, int playerX, i
     return foodLoc;
 }
 
+void gameOver(SDL_Renderer* renderer, SDL_Event event, int scale, int wScale, int tailLength) {
+    SDL_Color Red = { 255, 0, 0 };
+    SDL_Color White = { 255, 255, 255 };
+    SDL_Color Black = { 0, 0, 0 };
+
+    // Get the font used for displaying text
+    TTF_Font* font = TTF_OpenFont("arial.ttf", 10);
+    if (font == NULL) {
+        cout << "Font loading error: " << TTF_GetError() << endl;
+        return;
+    }
+
+    SDL_Surface* gameover = TTF_RenderText_Solid(font, "Game Over", Red);
+    SDL_Surface* retry = TTF_RenderText_Solid(font, "Press Enter to retry", White);
+    SDL_Surface* score = TTF_RenderText_Solid(font, (string("Score: ") + to_string(tailLength * 10)).c_str(), Black);
+    SDL_Texture* gameoverMessage = SDL_CreateTextureFromSurface(renderer, gameover);
+    SDL_Texture* retryMessage = SDL_CreateTextureFromSurface(renderer, retry);
+    SDL_Texture* scoreMessage = SDL_CreateTextureFromSurface(renderer, score);
+    SDL_Rect gameoverRect;
+    SDL_Rect retryRect;
+    SDL_Rect scoreRect;
+    gameoverRect.w = 200;
+    gameoverRect.h = 100;
+    gameoverRect.x = ((scale*wScale) / 2)-(gameoverRect.w/2);
+    gameoverRect.y = ((scale*wScale) / 2)-(gameoverRect.h/2)-50;
+    retryRect.w = 300;
+    retryRect.h = 50;
+    retryRect.x = ((scale*wScale) / 2) - ((retryRect.w / 2));
+    retryRect.y = (((scale*wScale) / 2) - ((retryRect.h / 2))+150);
+    scoreRect.w = 100;
+    scoreRect.h = 30;
+    scoreRect.x = ((scale*wScale) / 2) - (scoreRect.w / 2);
+    scoreRect.y = 0;
+    SDL_RenderCopy(renderer, gameoverMessage, NULL, &gameoverRect);
+    SDL_RenderCopy(renderer, retryMessage, NULL, &retryRect);
+    SDL_RenderCopy(renderer, scoreMessage, NULL, &scoreRect);
+
+    TTF_CloseFont(font);
+
+    // Show game over screen while space has not been pressed
+    while (true) {
+        SDL_RenderPresent(renderer);
+
+        if (SDL_PollEvent(&event)) {
+
+            if (event.type == SDL_QUIT) {
+                exit(0);
+            }
+
+            if (event.key.keysym.scancode == SDL_SCANCODE_RETURN) {
+                return;
+            }
+
+        }
+
+    }
+}
+
 int main(int argc, char* argv[]) {
     // Init everything
-    SDL_Init(SDL_INIT_EVERYTHING);
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        cout << "SDL initialization error: " << SDL_GetError() << endl;
+        return 1;
+    }
+
+    // Initialize SDL_ttf
+    if (TTF_Init() == -1) {
+        cout << "SDL_ttf initialization error: " << TTF_GetError() << endl;
+        return 1;
+    }
 
     SDL_Window* window;
     SDL_Renderer* renderer;
@@ -238,6 +332,7 @@ int main(int argc, char* argv[]) {
         //if snake has collided with a tail block, game over
         for (int i = 0; i < tailLength; i++) {
             if (x == tailX[i] && y == tailY[i]) {
+                gameOver(renderer, event, scale, wScale, tailLength);
                 x = 0;
                 y = 0;
                 up = false;
@@ -276,7 +371,21 @@ int main(int argc, char* argv[]) {
             y=scale*wScale-scale;
         }
         // Game over if snake collide with the walls
-        if ((x == 72 && y == 72)||(x == 96 && y == 72)||(x == 120 && y == 72)||(x == 144 && y == 72)||(x == 72 && y == 96)||(x == 72 && y == 120)||(x == 72 && y == 144)||(x == 480 && y == 72)||(x == 456 && y == 72)||(x == 432 && y == 72)||(x == 408 && y == 72)||(x == 480 && y == 96)||(x == 480 && y == 120)||(x == 480 && y == 144)||(x == 72 && y == 480)||(x == 96 && y == 480)||(x == 120 && y == 480)||(x == 144 && y == 480)||(x == 72 && y == 456)||(x == 72 && y == 432)||(x == 72 && y == 408)||(x == 480 && y == 480)||(x == 456 && y == 480)||(x == 432 && y == 480)||(x == 408 && y == 480)||(x == 480 && y == 456)||(x == 480 && y == 432)||(x == 480 && y == 408)) {         
+        if ((x == 72 && y == 72)||(x == 96 && y == 72)||
+        (x == 120 && y == 72)||(x == 144 && y == 72)||
+        (x == 72 && y == 96)||(x == 72 && y == 120)||
+        (x == 72 && y == 144)||(x == 480 && y == 72)||
+        (x == 456 && y == 72)||(x == 432 && y == 72)||
+        (x == 408 && y == 72)||(x == 480 && y == 96)||
+        (x == 480 && y == 120)||(x == 480 && y == 144)||
+        (x == 72 && y == 480)||(x == 96 && y == 480)||
+        (x == 120 && y == 480)||(x == 144 && y == 480)||
+        (x == 72 && y == 456)||(x == 72 && y == 432)||
+        (x == 72 && y == 408)||(x == 480 && y == 480)||
+        (x == 456 && y == 480)||(x == 432 && y == 480)||
+        (x == 408 && y == 480)||(x == 480 && y == 456)||
+        (x == 480 && y == 432)||(x == 480 && y == 408)) {    
+             gameOver(renderer, event, scale, wScale, tailLength);
             x = 0;
             y = 0;
             up = false;
@@ -362,12 +471,7 @@ int main(int argc, char* argv[]) {
         // Render food, player, score, and window border
         renderFood(renderer, food);
         renderPlayer(renderer, player, x, y, scale, tailX, tailY, tailLength);
-        renderScore(tailLength);
-
-        SDL_RenderDrawLine(renderer, 0, 0, 0, 24 * 24);
-        SDL_RenderDrawLine(renderer, 0, 24 * 24, 24 * 24, 24 * 24);
-        SDL_RenderDrawLine(renderer, 24 * 24, 24 * 24, 24 * 24, 0);
-        SDL_RenderDrawLine(renderer, 24 * 24, 0, 0, 0);
+        renderScore(renderer, tailLength, scale, wScale);
 
         // Put everything on screen
         SDL_RenderPresent(renderer);
